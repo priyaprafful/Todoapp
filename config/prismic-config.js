2de141300169ee5a17fa2fac08f87439b0b87943
Priyanka Maheshwari <priyankamaheshwari@Priyankas-MacBook-Pro.local>
2019-11-28 12:55:12 +0100

    accestoken is uncommented

---
+++ config/prismic-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apiEndpoint: 'https://todo-app-sample.cdn.prismic.io/api/v2',
+ 
+
+//-- Access token if the Master is not open
+ accessToken: '',
+ clientId: '',
+ clientSecret: '',
+
+// -- Links resolution rules
+// This function will be used to generate links to Prismic.io documents
+// As your project grows, you should update this function according to your routes
+
+  linkResolver: function(doc) {
+    if (doc.type == 'page') {
+      return `/${doc.lang}/${doc.uid}`;
+    } else {
+      return `/${doc.lang}`;
+    }
+  }
+}
