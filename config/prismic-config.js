42aa89f927385115273c1d130e6d484c1293f9b1
Priyanka Maheshwari <priyankamaheshwari@Priyankas-MacBook-Pro.local>
2019-11-28 17:42:12 +0100

    Prismic endpoint as your-repo-name

--- config/prismic-config.js
+++ config/prismic-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  apiEndpoint: 'https://todo-app-sample.cdn.prismic.io/api/v2',
+  apiEndpoint: 'https://your-repo-name.cdn.prismic.io/api/v2',
  
 
 //-- Access token if the Master is not open
