f90a26e05aec3f60dec9fe0a4a10ea3113939328
Priyanka Maheshwari <priyankamaheshwari@Priyankas-MacBook-Pro.local>
2019-12-06 13:22:55 +0100

    remove space between function and bracket

--- utils/async-handler.js
+++ utils/async-handler.js
@@ -1,10 +1,10 @@
 const asyncHandler = (callback) => {
   return async (req, res, next) => {
     try {
-      return await callback (req, res, next);
+      return await callback(req, res, next);
     } catch(err) {
       console.error(err);
-      res.render ("./error_handlers/prismic-error");
+      res.render("./error_handlers/error");
     }
   }
 }
